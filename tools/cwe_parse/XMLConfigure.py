9d2b90d5539fb755a122b9378ef33881c8adcb2d
kamil <kamilsh2007@gmail.com>
2026-05-21 23:53:02 +0300

    Распарсил всю иерархию CWE. Теперь в каждом для каждого CWE_ID можно получить его сыновей и детей на всех уровнях вложенности

---
+++ tools/cwe_parse/XMLConfigure.py
@@ -0,0 +1,52 @@
+import xml.etree.ElementTree as ET
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+XML_path = BASE_DIR / 'cwec_v4.20.xml'
+
+def get_highest_parents_for_view(xml_path, target_view="1000"):
+    ns = {'cwe': 'http://cwe.mitre.org/cwe-7'}
+
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+
+    parents_map = {}
+    kids_map = {}
+    for weakness in root.findall('.//cwe:Weaknesses/cwe:Weakness', ns):
+        w_id = weakness.attrib.get('ID')
+        for related in weakness.findall('.//cwe:Related_Weaknesses/cwe:Related_Weakness', ns):
+            if related.attrib.get('View_ID') == target_view and related.attrib.get('Nature') == 'ChildOf':
+                parent_id = related.attrib.get('CWE_ID')
+                kids_map.setdefault(parent_id, set()).add(w_id)
+
+                parents_map.setdefault(w_id, set()).add(parent_id)
+    containers = root.findall('.//cwe:Categories/cwe:Category', ns) + root.findall('.//cwe:Views/cwe:View', ns)
+    for container in containers:
+        parent_id = container.attrib.get('ID')
+        for member in container.findall('.//cwe:Relationships/cwe:Has_Member', ns):
+            if member.attrib.get('View_ID') == target_view:
+                child_id = member.attrib.get('CWE_ID')
+                kids_map.setdefault(parent_id, set()).add(child_id)
+                parents_map.setdefault(child_id, set()).add(parent_id)
+    return parents_map, kids_map
+
+Parents, Kids = get_highest_parents_for_view(XML_path, target_view="1000")
+
+def get_all_ancestors(cwe_id: str, parents: dict[str, list[str]], height: int = 0) -> dict[str, list[str]]:
+    ans = []
+    for parent_id in parents[cwe_id]:
+        ans.append((parent_id, height + 1))
+    for parent_id in parents[cwe_id]:
+        if parent_id not in parents:
+            continue
+        ans += get_all_ancestors(parent_id, parents, height + 1)
+    return ans
+def get_all_descendants(cwe_id: str, kids: dict[str, list[str]], height: int = 0) -> dict[str, list[str]]:
+    ans = []
+    for kid_id in kids[cwe_id]:
+        ans.append((kid_id, height + 1))
+    for kid_id in kids[cwe_id]:
+        if kid_id not in mapping:
+            continue
+        ans += get_all_descendants(kid_id, mapping, height + 1)
+    return
---
+++ tools/cwe_parse/cwe_parse.py
@@ -0,0 +1,45 @@
+from XMLConfigure import Parents, Kids, get_all_ancestors, get_all_descendants
+
+class CWENode:
+    def __init__(self, cwe_id: str):
+        self.cwe_id = cwe_id
+
+        self.ancestors_dict: dict[str, list[str]] = {}
+        self.descendants_dict: dict[str, list[str]] = {}
+
+        ancestors: list[tuple[str, int]] = get_all_ancestors(self.cwe_id, Parents)
+        descedants: list[tuple[str, int]] = get_all_descendants(self.cwe_id, Kids)
+
+        self.ancestors_dict["ALL"] = []
+        self.descendants_dict["ALL"] = []
+
+        for ancestor_id, height in ancestors:
+            self.ancestors_dict["ALL"].append(ancestor_id)
+            if str(height) not in ancestors_dict:
+                self.ancestors_dict[str(height)] = []
+            self.ancestors_dict[str(height)].append(ancestor_id)
+        for descedant_id, height in descedants:
+            self.descendants_dict["ALL"].append(descedant_id)
+            if str(height) not in descendants_dict:
+                self.descendants_dict[str(height)] = []
+            self.descendants_dict[str(height)].append(descedant_id)
+
+    def get_ancestors(self, level: str = None):
+        if level is None:
+            return self.ancestors_dict["ALL"]
+        return self.ancestors_dict[level]
+    def get_descendants(self, level: str = None):
+        if level is None:
+            return self.descendants_dict["ALL"]
+        return self.descendants_dict[level]
+
+# Необходимо создать локальный файл (он почти не обновляется, смысла в хранении в БД нет)
+#
+# Там прописать для каждой вершины все уровни потомков, предков
+# Чтобы все это работало быстро, в файлике все уровни вложенности уже прописываем
+
+# P.S. будет достаточно мало записей, т.к. на каждом уровне нодов суммарно
+# хранится не более тысячи записей
+
+# Нужно сделать через @property get_descendants(level=None), get_ancestors(level=None), которые
+# по уровню level: str будут давать все эти списки нодов (только их cwe_id, прочее не нужно)
