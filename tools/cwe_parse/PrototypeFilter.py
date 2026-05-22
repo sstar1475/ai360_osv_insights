0a1f681e83171f80ce7bb6800f49ea66f7492b66
kamil <kamilsh2007@gmail.com>
2026-05-22 17:54:16 +0300

    Убрал проверку

--- tools/cwe_parse/PrototypeFilter.py
+++ tools/cwe_parse/PrototypeFilter.py
@@ -1,14 +1,9 @@
 import asyncio
 import sys
-from pathlib import Path
+import pandas as pd
 from cwe_parse import CWENode
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-sys.path.insert(0, str(BASE_DIR))
-from db.OSVDataClient import OSVDataClient
 
-df = OSVDataClient()
-
-async def filter(df_report, cwe_id: str):
+async def filter(df_report: pd.DataFrame, cwe_id: str) -> pd.DataFrame:
     node = CWENode(cwe_id)
     _kids = node.get_descendants()
     kids = []
@@ -16,9 +11,3 @@ async def filter(df_report, cwe_id: str):
         kids.append(f"CWE-{kid}")
 
     return df_report[df_report["vulnerability_cwe_id"].isin(kids)]
-async def _db_():
-    return await df.get_detailed_report()
-df_rep = asyncio.run(_db_())
-if __name__ == "__main__":
-    ds = asyncio.run(filter(df_rep, "284"))
-    print(ds)
